efbf4b716a1fff520469fab320366b86bc46e64e
HUANG JING CHAO <91250467+rafa920613@users.noreply.github.com>
2021-10-07 17:39:58 +0800

    Add files via upload

---
+++ weak04/weak04-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a=123,b=10;
+    float  c=3.1415926538979;
+    double d=3.1415926538979;
+    printf("%f\n",c);
+    printf("%f\n",d);
+    printf("%.4f\n",d);
+    printf("%.9f\n",d);
+    printf("%.13f\n",d);
+}
+
