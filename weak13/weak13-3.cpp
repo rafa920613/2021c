e308d8ff1aa026fdfdd51fef947a6a1de63092df
rafa920613 <huangjingchao2@gmail.com>
2022-01-06 17:28:15 +0800

    add week3

--- weak13/weak13-3.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void printfstar(int n)
-{
-    for(int i=0;i<n;i++) printf("*");
-}
-int main()
-{
-    printfstar(7);
-}
---
+++ week07/weak07-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n=3;
+
+    while(n>0){
+        printf("while(判斷n>0)有進來喔,n:%d\n",n);
+
+        n--;
+    }
+
+    printf("最後順利離開while(迴圈),n:%d\n",n);
+}
